636ee52bbd8254d3e7aa20915d65437ab77430a3
최기환 <rlghks3004@gmail.com>
2023-12-30 20:28:20 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ['shop-phinf.pstatic.net'],
+    remotePatterns: [
+      {
+        hostname: 'shop-phinf.pstatic.net',
+      },
+    ],
   },
 };
 
