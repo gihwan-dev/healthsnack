40788b119290b815de07c9c6f5f524df1bbe65f4
최기환 <rlghks3004@gmail.com>
2023-12-30 22:33:58 +0900

    .

--- src/features/footer/components/FooterPersonal.tsx
+++ src/features/footer/components/FooterPersonal.tsx
@@ -1,5 +1,6 @@
 import InstagramSVG from '@/assets/images/instagram';
 import { footerText, footerTextContainer } from '../style';
+import Link from 'next/link';
 
 const FooterPersonal = () => {
   return (
@@ -8,10 +9,13 @@ const FooterPersonal = () => {
       <li className={footerText}>대표자: 박시현</li>
       <li className={footerText}>고객센터: 010-5069-5364</li>
       <li className={footerText}>이메일: edo68@naver.com</li>
-      <li className={footerText + ' flex flex-row gap-1'}>
+      <Link
+        href="https://www.instagram.com/healthsnack_meat?utm_source=ig_web_button_share_sheet&igsh=OGQ5ZDc2ODk2ZA=="
+        className={footerText + ' flex flex-row gap-1'}
+      >
         <InstagramSVG />
         healthsnack_meat
-      </li>
+      </Link>
     </ul>
   );
 };
